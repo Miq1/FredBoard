3db2111c40a35684a494806918c4f98c09795530
Miq1 <miq1@gmx.de>
2024-09-16 10:48:00 +0200

    Complete playground script

--- Sensitivity/playground.pl
+++ Sensitivity/playground.pl
@@ -4,17 +4,19 @@ use Data::Dumper;
 
 my $center;
 my $spread;
-my $rule;
+my $steepness;
 my $reduce = 1;
+my $samples;
 
-die "Usage: playground.pl <center> <spread> <rule>\n" unless $#ARGV == 2;
+die "Usage: playground.pl <center> <spread> <steepness> [<samples>]\n" unless $#ARGV >= 2;
 
-($center, $spread, $rule) = @ARGV;
+($center, $spread, $steepness, $samples) = @ARGV;
 die "center must be [0..4095]\n" unless $center >= 0 && $center <= 4095;
 die "spread must be [0..255]\n" unless $spread >= 0 && $spread <= 255;
-die "rule must be float [0..31]\n" unless $ rule >= 0 && $ rule <= 31;
+die "steepness must be float [-31..31]\n" unless $ steepness >= -31 && $ steepness <= 31;
 
 my @correction;
+my @linear;
 my $val;
 # Set initial even distribution as default
 for (my $i = 0; $i < 4096; $i++) {
@@ -23,6 +25,7 @@ for (my $i = 0; $i < 4096; $i++) {
     $val++;
   }
 }
+@linear = @correction;
 
 # If we have a center value, we will adapt the distribution
 if ($center) {
@@ -49,7 +52,7 @@ if ($center) {
     # now reduce the spread
     $spread -= $reduce;
     # dynamically shorten intervals
-    $reduce *= $rule;
+    $reduce += $steepness;
     # get new left and right boundaries
     $left_bound -= $spread;
     $right_bound += $spread;
@@ -58,7 +61,8 @@ if ($center) {
     $lvalue = 0 if $lvalue < 0;
     $rvalue++; 
     $rvalue = 127 if $rvalue > 127;
-  } while ($spread > 0 && $left_bound >= 0 && $right_bound <= 4095);
+  } while ($left_bound+$spread >= 0 && $right_bound-$spread <= 4095);
+  # } while ($spread > 0 && $left_bound >= 0 && $right_bound <= 4095);
 }
 
 # Print out result
@@ -68,11 +72,15 @@ for (my $i = 0; $i < 4096; $i++) {
 }
 
 my @statistics;
-while (<STDIN>) {
-  (my $val) = $_ + 0;
+my @linear_statistics;
+my $statcount;
+while (<STDIN> && ($samples == 0 || $statcount < $samples)) {
+  (my $val) = $_=~/(\d+)/;
+  $statcount++;
   $statistics[$correction[$val]]++;
+  $linear_statistics[$correction[$val]]++;
 }
 
 for(my $i = 0; $i < @statistics; $i++) {
-  printf("%3d: %d\n", $i, $statistics[$i]);
+  printf("%3d; %5.2f; %5.2f\n", $i, $statistics[$i]/$statcount*100.0, $linear_statistics[$i]/$statcount*100.0);
 }
